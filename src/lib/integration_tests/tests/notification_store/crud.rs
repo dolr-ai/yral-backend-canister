de23f777d67f57845e300e87ef8928a76abd43f4
JoeruCodes <joymedi8143@gmail.com>
2025-07-14 15:22:04 +0530

    Refactor get_notifications query calls in CRUD and guard tests to use reference parameters
    - Updated the `get_notifications` query calls in both `test_crud` and `test_guard` functions to use reference parameters in the `Encode!` macro, enhancing consistency and clarity in the encoding process.

--- src/lib/integration_tests/tests/notification_store/crud.rs
+++ src/lib/integration_tests/tests/notification_store/crud.rs
@@ -22,7 +22,7 @@ fn test_crud() {
     };
     res.unwrap();
 
-    let notifications = pic.query_call(notification_store_canister_id, alice_principal, "get_notifications", Encode!(10, 0).unwrap()).unwrap();
+    let notifications = pic.query_call(notification_store_canister_id, alice_principal, "get_notifications", Encode!(&10, &0).unwrap()).unwrap();
     let notifications: Vec<NotificationData> = match notifications {
         WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
         _ => panic!("\n🛑 get notifications failed\n"),
@@ -40,7 +40,7 @@ fn test_crud() {
     };
     res.unwrap();
 
-    let notifications = pic.query_call(notification_store_canister_id, alice_principal, "get_notifications", Encode!(10, 0).unwrap()).unwrap();
+    let notifications = pic.query_call(notification_store_canister_id, alice_principal, "get_notifications", Encode!(&10, &0).unwrap()).unwrap();
     let notifications: Vec<NotificationData> = match notifications {
         WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
         _ => panic!("\n🛑 get notifications failed\n"),
