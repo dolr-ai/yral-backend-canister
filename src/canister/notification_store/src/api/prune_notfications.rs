d0e926213f46b39fef755b8986136336b6b2985c
joel-medicala-yral <joel@gobazzinga.io>
2025-07-24 09:02:24 +0530

    Update src/canister/notification_store/src/api/prune_notfications.rs
    Co-authored-by: Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>

--- src/canister/notification_store/src/api/prune_notfications.rs
+++ src/canister/notification_store/src/api/prune_notfications.rs
@@ -11,19 +11,21 @@ pub fn prune_notifications() {
     CANISTER_DATA.with_borrow_mut(|map| {
         let now = get_current_system_time_from_ic();
         // Keep notifications from the last 30 days
-        let thirty_days = Duration::from_secs(30 * 24 * 60 * 60);
-        let thirty_days_ago = now.checked_sub(thirty_days).unwrap_or(std::time::UNIX_EPOCH);
+           let thirty_days = Duration::from_secs(30 * 24 * 60 * 60);
+        let thirty_days_ago = now
+            .checked_sub(thirty_days)
+            .unwrap_or(std::time::UNIX_EPOCH);
 
-        // Collecting the user principals first to avoid borrowing issues while mutating the map
-        let users: Vec<Principal> = map.notifications.iter().map(|(user, _)| user).collect();
+        let mut notifications: Vec<(Principal, Notification)> = map.notifications.iter().collect();
 
-        for user in users {
-            if let Some(mut notifications) = map.notifications.get(&user) {
-                // Keep notifications that were created after thirty_days_ago
-                notifications.0.retain(|n| n.created_at > thirty_days_ago);
+        notifications
+            .iter_mut()
+            .for_each(|(principal, notification)| {
+                notification
+                    .0
+                    .retain(|data| data.created_at >= thirty_days_ago);
 
-                map.notifications.insert(user, notifications);
-            }
-        }
-    })
+                map.notifications.insert(*principal, notification.clone());
+            });
+    });
 }
