eb16eed3db7f1f31eef9b355e2b4a001e75751bf
joel-medicala-yral <joel@gobazzinga.io>
2025-07-24 10:39:00 +0530

    feat: add prune_notifications function to notification_store canister (#599)
    * feat: add prune_notifications function to notification_store canister
    
    - Introduced a new `prune_notifications` function to the Candid interface for managing notification cleanup.
    - Removed the `set_pruning_timer` function from the initialization and post-upgrade processes to streamline notification management.
    - Updated relevant module imports to reflect the removal of the pruning timer functionality.
    
    * feat: update prune_notifications function to retain recent notifications
    
    - Modified the `prune_notifications` function to keep notifications created within the last 30 days, enhancing the notification management logic.
    - Added integration tests to verify the functionality of the `prune_notifications` method, ensuring it correctly retains recent notifications and handles unauthorized access attempts.
    
    * fix: update parameter encoding in prune_notifications tests
    
    - Changed the parameter encoding in the `prune_notifications` function calls within the integration tests to use `candid::encode_args(())` instead of an empty vector, ensuring proper argument handling and consistency in test execution.
    
    * refactor: update user principal in prune_notifications test
    
    - Changed the principal used in the `prune_notifications` test from a global super admin to a regular user, enhancing the test's focus on unauthorized access scenarios and ensuring it accurately reflects user permissions.
    
    * Update src/canister/notification_store/src/api/prune_notfications.rs
    
    Co-authored-by: Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
    
    * fix: add missing import for Notification type in prune_notifications
    
    ---------
    
    Co-authored-by: JoeruCodes <joymedi8143@gmail.com>
    Co-authored-by: Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>

--- src/canister/notification_store/src/canister_lifecycle/init.rs
+++ src/canister/notification_store/src/canister_lifecycle/init.rs
@@ -1,12 +1,11 @@
 use ic_cdk_macros::init;
 use shared_utils::canister_specific::notification_store::types::args::NotificationStoreInitArgs;
 
-use crate::{set_pruning_timer, CANISTER_DATA};
+use crate::CANISTER_DATA;
 
 #[init]
 fn init(args: NotificationStoreInitArgs) {
     CANISTER_DATA.with_borrow_mut(|canister_data| {
         canister_data.version = args.version;
     });
-    set_pruning_timer();
 }
