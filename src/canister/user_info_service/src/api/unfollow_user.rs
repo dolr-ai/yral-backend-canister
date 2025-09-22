0baf7f3da51c953b93987e8d185acee3d229e2e5
Komal <157946119+komal-sai-yral@users.noreply.github.com>
2025-09-22 11:37:26 +0530

    feat: remove anonymous condition for profile calls (#618)
    rm anon

--- src/canister/user_info_service/src/api/unfollow_user.rs
+++ src/canister/user_info_service/src/api/unfollow_user.rs
@@ -1,13 +1,10 @@
 use candid::Principal;
 use ic_cdk::caller;
 use ic_cdk_macros::update;
-use shared_utils::common::utils::permissions::is_not_anonymous;
 
 use crate::CANISTER_DATA;
 
-#[update(guard = "is_not_anonymous")]
+#[update]
 fn unfollow_user(target: Principal) -> Result<(), String> {
-    CANISTER_DATA.with_borrow_mut(|canister_data| {
-        canister_data.unfollow_user(caller(), target)
-    })
+    CANISTER_DATA.with_borrow_mut(|canister_data| canister_data.unfollow_user(caller(), target))
 }
--- src/canister/user_info_service/src/api/update_profile_details.rs
+++ src/canister/user_info_service/src/api/update_profile_details.rs
@@ -1,13 +1,11 @@
 use ic_cdk::caller;
 use ic_cdk_macros::update;
-use shared_utils::common::utils::permissions::is_not_anonymous;
 
-use crate::data_model::ProfileUpdateDetails;
 use crate::CANISTER_DATA;
+use crate::data_model::ProfileUpdateDetails;
 
-#[update(guard = "is_not_anonymous")]
+#[update]
 fn update_profile_details(details: ProfileUpdateDetails) -> Result<(), String> {
-    CANISTER_DATA.with_borrow_mut(|canister_data| {
-        canister_data.update_profile_details(caller(), details)
-    })
+    CANISTER_DATA
+        .with_borrow_mut(|canister_data| canister_data.update_profile_details(caller(), details))
 }
