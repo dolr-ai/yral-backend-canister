4a4bb995e3cdc514a2c3a64b68c52278253a7165
komal-sai-yral <komalsai@gobazzinga.io>
2025-09-22 09:12:35 +0530

    rm anon

--- src/canister/user_info_service/src/api/follow_user.rs
+++ src/canister/user_info_service/src/api/follow_user.rs
@@ -1,13 +1,10 @@
 use candid::Principal;
 use ic_cdk::caller;
 use ic_cdk_macros::update;
-use shared_utils::common::utils::permissions::is_not_anonymous;
 
 use crate::CANISTER_DATA;
 
-#[update(guard = "is_not_anonymous")]
+#[update]
 fn follow_user(target: Principal) -> Result<(), String> {
-    CANISTER_DATA.with_borrow_mut(|canister_data| {
-        canister_data.follow_user(caller(), target)
-    })
+    CANISTER_DATA.with_borrow_mut(|canister_data| canister_data.follow_user(caller(), target))
 }
