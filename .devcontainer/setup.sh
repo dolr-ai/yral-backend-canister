ccca8ee8ac61f2db6d4d689cce12585eb3a03c84
Saikat Das <saikatdas0790@gmail.com>
2025-10-14 14:19:55 +0000

    feat: enhance devcontainer setup with environment variable and updated setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,5 +22,9 @@
 # chmod +x .cargo-husky/hooks/*;
 # # Git hook for cargo husky gets registered with cargo test. Please run
 # # cargo test --all-features;
-DFXVM_INIT_YES=true sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)" &&
-source "$HOME/.local/share/dfx/env"
+DFXVM_INIT_YES=true sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)";
+source "$HOME/.local/share/dfx/env";
+
+# set to pwd
+pwd;
+echo $POCKET_IC_BIN;
