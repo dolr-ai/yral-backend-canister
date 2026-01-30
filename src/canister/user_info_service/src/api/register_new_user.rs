ddb53d7f14f6dcc2c8d43fe4becc3a37fa4cec01
joel-medicala-yral <joel@gobazzinga.io>
2026-01-30 15:39:06 +0530

    Revert "fix register_new_user_v2"
    This reverts commit 74913b1c3e977212fbd499719301baa60bfab061.

--- src/canister/user_info_service/src/api/register_new_user.rs
+++ src/canister/user_info_service/src/api/register_new_user.rs
@@ -1,4 +1,3 @@
-use candid::Principal;
 use ic_cdk::caller;
 use ic_cdk_macros::update;
 use shared_utils::common::utils::permissions::is_not_anonymous;
@@ -8,11 +7,4 @@ use crate::CANISTER_DATA;
 #[update(guard = "is_not_anonymous")]
 fn register_new_user() -> Result<(), String> {
     CANISTER_DATA.with_borrow_mut(|canister_data| canister_data.register_new_user(caller()))
-}
-
-#[update(guard = "is_not_anonymous")]
-fn register_new_user_v2(bot_account_principal: Option<Principal>) -> Result<(), String> {
-    CANISTER_DATA.with_borrow_mut(|canister_data| {
-        canister_data.register_new_user_v2(bot_account_principal)
-    })
 }
