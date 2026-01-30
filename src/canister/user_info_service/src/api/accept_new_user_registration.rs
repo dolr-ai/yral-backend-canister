eac7abb10a26b06d69f8e8602d7ab66c473cc459
joel-medicala-yral <joel@gobazzinga.io>
2026-01-30 15:03:07 +0530

    fix: accept new registration guard

--- src/canister/user_info_service/src/api/accept_new_user_registration.rs
+++ src/canister/user_info_service/src/api/accept_new_user_registration.rs
@@ -1,6 +1,6 @@
 use candid::Principal;
 use ic_cdk::update;
-use shared_utils::common::utils::permissions::is_caller_global_admin;
+use shared_utils::common::utils::permissions::{is_caller_global_admin, is_not_anonymous};
 
 use crate::CANISTER_DATA;
 
@@ -11,12 +11,18 @@ fn accept_new_user_registration(user_principal: Principal, authenticated: bool) 
     })
 }
 
-#[update(guard = "is_caller_global_admin")]
+#[update(guard = "is_not_anonymous")]
 fn accept_new_user_registration_v2(
     new_principal: Principal,
     authenticated: bool,
     main_account: Option<Principal>,
 ) -> Result<(), String> {
+    if let Some(main_account_principal) = main_account {
+        if ic_cdk::caller() != main_account_principal {
+            return Err("Caller does not match main account".to_string());
+        }
+    }
+
     CANISTER_DATA.with_borrow_mut(|canister_data| {
         canister_data.register_authenticated_user_v2(new_principal, authenticated, main_account)
     })
